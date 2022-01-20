052beee6a1b26c4f33807e479cd89c97ab2c685d
AMANGUPTA <aman.180410116028@gmail.com>
2022-01-20 16:24:57 +0530

    php converted

---
+++ includes/footer.php
@@ -0,0 +1,132 @@
+<!-- Get In Touch Section Begin -->
+    <div class="gettouch-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="gt-text">
+                        <i class="fa fa-map-marker"></i>
+                        <p><a href="https://goo.gl/maps/vQk8xA7kKdyfh7xY7" target="_blank" style="color:#fff">SVIT Road, Rajupura Village, Vasad, Anand, Gujarat 388306</p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="gt-text">
+                        <i class="fa fa-mobile"></i>
+                        <ul>
+                        <li><a href="tel:8490074656" style="color:#fff">849-007-4656</a></li>
+                        <li><a href="tel:9723964838" style="color:#fff">972-396-4838</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="gt-text email">
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="mailto:info.stayfithealth@gmail.com" target="_blank" style="color:#fff">info.stayfithealth@gmail.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- Get In Touch Section End -->
+<!-- Footer Section Begin -->
+<section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="fs-about">
+                        <div class="fa-logo">
+                            <a href="#"><img src="img/checklogobg.png" alt="" height="120"></a>
+                        </div>
+                        <p>StayFit is a concept that will help people to maintain their fitness with their routine life.</p>
+                        <div class="fa-social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-youtube-play"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa  fa-envelope-o"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6">
+                    <div class="fs-widget">
+                        <h4>Useful links</h4>
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Classes</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-2 col-md-3 col-sm-6">
+                    <div class="fs-widget">
+                        <h4>Support</h4>
+                        <ul>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">My account</a></li>
+                            <li><a href="#">Subscribe</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="fs-widget">
+                        <h4>Tips & Guides</h4>
+                        <div class="fw-recent">
+                            <h6><a href="#">Physical fitness may help prevent depression, anxiety</a></h6>
+                            <ul>
+                                <!-- <li>3 min read</li>
+                                <li>20 Comment</li> -->
+                            </ul>
+                        </div>
+                        <div class="fw-recent">
+                            <h6><a href="#">Fitness: The best exercise to lose belly fat and tone up...</a></h6>
+                            <ul>
+                                <!-- <li>3 min read</li>
+                                <li>20 Comment</li> -->
+                            </ul>
+                        </div>
+                        <div class="fw-recent">
+                            <h6><a href="#">Regular physical activity can improve your muscle strength and boost your endurance.</a></h6>  
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="copyright-text">
+                        <p>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This website is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="" target="_blank">Team-ADJN</a><center><div class="hide" style="border-top: 5px solid gray;">1) Aman Gupta 2) Dhruv Gandhi</div>
+  <div class="hide" style="border-bottom: 5px solid gray;">3) Jathin Prakash 4) Neehal Hansoti</div></center></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Footer Section End -->
+
+    <!-- Search model Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search model end -->
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+
+
+
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="StayFit - Your Guide to a Fit Life">
+    <meta name="keywords" content="Gym, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="img/main-logo-bg.png" type="image/x-icon">
+    <title>Stay-Fit</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader">
+ 
+        </div>
+        <div clas="copyright-text" style="color: rgb(243,97,0);position: fixed;top: 70%;left: 45%;transform: translate(-50%, -50%);"><a href="https://amangupta2000.github.io/" target="_blank" rel="noopener noreferrer"></a>Aman Gupta</div>
+        <div clas="copyright-text" style="color: rgb(243,97,0);position: fixed;top: 70%;left: 55%;transform: translate(-50%, -50%);">Dhruv Gandhi</div>
+        <div clas="copyright-text" style="color: rgb(243,97,0);position: fixed;top: 80%;left: 45%;transform: translate(-50%, -50%);">Jathin Prakash</div>
+        <div clas="copyright-text" style="color: rgb(243,97,0);position: fixed;top: 80%;left: 55%;transform: translate(-50%, -50%);">Neehal Hansoti</div>
+    </div>
+
+    <!-- Offcanvas Menu Section Begin -->
+    <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div class="canvas-close">
+            <i class="fa fa-close"></i>
+        </div>
+        <div class="canvas-search search-switch">
+            <i class="fa fa-search"></i>
+        </div>
+        <nav class="canvas-menu mobile-menu">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./about-us.php">About Us</a></li>
+                <li><a href="./classes.php">Classes</a></li>
+                <li><a href="./services.php">Services</a></li>
+                <!-- <li><a href="./team.php">Our Team</a></li> -->
+                <li><a href="#">Pages</a>
+                    <ul class="dropdown">
+                        <li><a href="./about-us.php">About us</a></li>
+                        <li><a href="./class-timetable.php">Classes timetable</a></li>
+                        <li><a href="./bmi-calculator.php">Bmi calculate</a></li>
+                        <!-- <li><a href="./team.php">Our team</a></li> -->
+                        <li><a href="./gallery.php">Gallery</a></li>
+                        <li><a href="./blog.php">Our blog</a></li>
+                        <li><a href="./404.php">404</a></li>
+                    </ul>
+                </li>
+                <li><a href="./contact.php">Contact</a></li>
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+        <!-- <div class="canvas-social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-youtube-play"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div> -->
+    </div>
+    <!-- Offcanvas Menu Section End -->
+
+    <!-- Header Section Begin -->
+    <header class="header-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="logo">
+                        <a href="./index.php">
+                            <img src="img/checklogobg.png" alt="" style="margin-top: -25px;margin-left: 60px;" height="120">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <nav class="nav-menu">
+                        <ul>
+                            <li class="active"><a href="./index.php">Home</a></li>
+                            <li><a href="./about-us.php">About Us</a></li>
+                            <li><a href="./class-details.php">Classes</a></li>
+                            <li><a href="./services.php">Services</a></li>
+                            <!-- <li><a href="./team.php">Our Team</a></li> -->
+                            <li><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="./about-us.php">About us</a></li>
+                                    <li><a href="./class-timetable.php">Classes timetable</a></li>
+                                    <li><a href="./bmi-calculator.php">Bmi calculate</a></li>
+                                    <!-- <li><a href="./team.php">Our team</a></li> -->
+                                    <li><a href="./gallery.php">Gallery</a></li>
+                                    <li><a href="./blog.php">Our blog</a></li>
+                                    <li><a href="./404.php">404</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="./contact.php">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-3">
+                    <div class="top-option">
+                        <div class="to-search search-switch">
+                            <i class="fa fa-search"></i>
+                        </div>
+                        <div class="to-social">
+                            <a href="get-started.php"><i class="fa fa-user"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="canvas-open">
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
